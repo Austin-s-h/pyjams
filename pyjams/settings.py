cfe25feb276ee20c8a32e0057a05e5d2cda0fd88
Austin Hovland <austin@sansterbioanalytics.com>
2024-12-26 14:53:00 -0500

    perf: logging

--- pyjams/settings.py
+++ pyjams/settings.py
@@ -283,3 +283,25 @@ if IS_HEROKU_APP:
         'https://*.herokuapp.com',
         'https://*.sansterbioanalytics.com',
     ])
+
+# Logging Configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
